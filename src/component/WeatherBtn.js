8ae2ddd803123dc71c32548d6d7d5cf58c80ee18
Sungmin-Kim-Dev <dreamerk24dev@gmail.com>
2024-08-19 01:00:37 +0900

    button color fix

--- src/component/WeatherBtn.js
+++ src/component/WeatherBtn.js
@@ -6,13 +6,18 @@ const WeatherBtn = ({cities, setCity, selectedCity}) => {
   return (
     <div className="btn-box grid grid-cols-5 gap-4">
       <button
-        className={`p-2 rounded-lg border border-zinc-600 drop-shadow-lg bg-blue-400/${!selectedCity ? 100 : 40} hover:bg-blue-400/100`}
+        className={`p-2 rounded-lg border border-zinc-600 drop-shadow-lg ${
+          !selectedCity ? 'bg-blue-400/100' : 'bg-blue-400/40'
+        } hover:bg-blue-400/100`}
         onClick={() => setCity('')}>
         Current Location
       </button>
       {cities.map((item) => (
         <button
-          className={`p-2 rounded-lg border border-zinc-600 drop-shadow-lg bg-blue-400/${selectedCity === item ? 100 : 40} hover:bg-blue-400/100`} key={item}
+          className={`p-2 rounded-lg border border-zinc-600 drop-shadow-lg ${
+            selectedCity === item ? 'bg-blue-400/100' : 'bg-blue-400/40'
+          } hover:bg-blue-400/100`}
+          key={item}
           onClick={() => setCity(item)}>
           {/* 도시 이름 단어 첫 글자 대문자 */}
           {item
